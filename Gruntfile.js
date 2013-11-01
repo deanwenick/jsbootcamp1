20ec47fa1dd2d662cbf3acaa155fb76019381134
Dean Wenick <deanwenick@gmail.com>
2013-11-01 15:19:09 -0700

    configured Gruntfile for copy, clean and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,31 @@
 module.exports = function(grunt) {
   // Do grunt-related things in here
   grunt.initConfig({
-  	pkg: grunt.file.readJSON( 'package.json' )
-  });
+  	pkg: grunt.file.readJSON( 'package.json' ),
+
+  	jshint: {
+  		files: ['app.js','Gruntfile.js']
+  	},
+
+  	copy: {
+  		main: {
+  			files: [{
+  				src: ['bower_components/**'],
+  				dest: 'public/'
+  			}]
+  		}
+  	},
+
+  	clean: ['public/bower_components/**']
+  });//initConfig
+
+  //load jshint plugin for cleaning js files
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  //load copy plugin to move files into public folder
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  //load clean plugin to clean bower components folder
+  grunt.loadNpmTasks( 'grunt-contrib-clean');
+
+  //default tasks for grunt
+  grunt.registerTask( 'default', ['clean','copy']);
 };
