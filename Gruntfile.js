0ecd957716a88378d87b58f723fbd69009c366b6
Dean Wenick <deanwenick@gmail.com>
2013-11-01 16:34:41 -0700

    added mocha-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,35 @@
 module.exports = function(grunt) {
   // Do grunt-related things in here
   grunt.initConfig({
-  	pkg: grunt.file.readJSON( 'package.json' ),
+    pkg: grunt.file.readJSON( 'package.json' ),
 
-  	jshint: {
-  		files: ['app.js','Gruntfile.js']
-  	},
+    jshint: {
+        files: ['app.js','Gruntfile.js']
+    },
 
-  	copy: {
-  		main: {
-  			files: [{
-  				src: ['bower_components/**'],
-  				dest: 'public/'
-  			}]
-  		}
-  	},
+    copy: {
+        main: {
+            files: [{
+            src: ['bower_components/**'],
+            dest: 'public/'
+            }]
+        }
+    },
 
-  	clean: ['public/bower_components/**']
+    clean: ['public/bower_components/**'],
+
+    mocha_phantomjs: {
+    	all: ['tests/test01.js']
+    },
+
+    mocha: {
+      index: ['tests/test01.js']
+    },
+
+    mochaTest: { test: { 
+		options: {reporter: 'spec'},
+		src: ['tests/test01.js']
+    }}
   });//initConfig
 
   //load jshint plugin for cleaning js files
@@ -24,9 +37,14 @@ module.exports = function(grunt) {
   //load copy plugin to move files into public folder
   grunt.loadNpmTasks( 'grunt-contrib-copy' );
   //load clean plugin to clean bower components folder
-  grunt.loadNpmTasks( 'grunt-contrib-clean');
+  grunt.loadNpmTasks( 'grunt-contrib-clean' );
+  grunt.loadNpmTasks( 'grunt-mocha-phantomjs' );
+  grunt.loadNpmTasks( 'mocha' );
+  grunt.loadNpmTasks( 'grunt-mocha-test' );
 
   //default tasks for grunt
   //* add jshint to this list *//
-  grunt.registerTask( 'default', ['clean','copy']);
+  //
+  grunt.registerTask( 'default', ['jshint','clean','copy']);
+  grunt.registerTask('test', ['mochaTest']);
 };
---
+++ tests/test01.js
@@ -0,0 +1,22 @@
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      //assert.equal(-1, [1,2,3].indexOf(0));
+      //assert.equal(0, [1,2,3].indexOf(1));
+    });
+
+    it("...", function() {
+        assert.equal(-1, [1,2,3].indexOf(0));
+    });
+
+    it("should return that each string is the same", function() {
+        assert.equal("a","a");
+    });
+
+    it("should return that each string is the same", function() {
+        assert.equal(100,100);
+    });
+  })
+})
